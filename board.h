c34dff150133ea8b10acfc34b5aecab9ca20bdfb
Kevin <pk585261@outlook.com>
2025-04-28 21:25:23 -0700

    code push, good night :)

--- board.h
+++ board.h
@@ -9,16 +9,19 @@ class Board{
     DynamicArray<DynamicArray<Cell>> cellMap;
 
     void generate(int, int);
+    bool moveUp();
+    bool moveDown();
+    bool moveLeft();
+    bool moveRight();
+
 
     public:
         Board();
         Board(int, int, int, int, DynamicArray<DynamicArray<Cell>>);
-        void setPlayerX(int);
-        void setPlayerY(int);
         void setPlayerHealth(int);
         void setTreasureCollected(int);
-        int getPlayerX();
-        int getPlayerY();
         int getPlayerHealth();
         int getTreasureCollected();
+        bool playGame();
+
 };
