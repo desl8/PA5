811bbca3fc92784651f6c33545207c4fdf475a09
Kevin <pk585261@outlook.com>
2025-05-04 09:54:39 -0700

    It doesn't even matter how hard you try

--- helpers.cpp
+++ helpers.cpp
@@ -11,13 +11,13 @@ std::ostream& operator<<(std::ostream& out, Cell& c){
 }
 
 std::ostream& operator<<(std::ostream& out, const Board& board){
-    for(int i = 0; i < board.cellMap.getCurrentNumElementsStored(); i++){
-        for(int j = 0; j < board.cellMap.getElementAtIndex(i).getCurrentNumElementsStored(); j++){
+    for(int i = 0; i < board.cellMap->getCurrentNumElementsStored(); i++){
+        for(int j = 0; j < board.cellMap->getElementAtIndex(i).getCurrentNumElementsStored(); j++){
             if(i == board.playerY && j == board.playerX){
                 out << "[ P ]"; // Player position
             }
             else{
-                out << *board.cellMap.getElementAtIndex(i).getElementAtIndex(j);
+                out << *board.cellMap->getElementAtIndex(i).getElementAtIndex(j);
             }
         }
         out << std::endl;
